dde6e110100b3bfa736d25c8d1cea0c9a452b924
PranavShadow <ompranav2003@gmail.com>
2025-10-27 18:21:57 +0530

    add gitignore

--- Functions/Prime.java
+++ Functions/Prime.java
@@ -1,6 +1,20 @@
-public package Functions;
+import java.util.Scanner;
 
-class Prime {
-    
-    
+public class Prime{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean isPrime = false;
+        for (int i = 2; i < n; i++) {
+            if (n%i==0) {
+                isPrime = true;
+            }
+        }
+        if (!isPrime) {
+            System.out.println(n + " is Prime");
+        }
+        else{
+            System.out.println(n + " is Not Prime");
+        }
+    }
 }
